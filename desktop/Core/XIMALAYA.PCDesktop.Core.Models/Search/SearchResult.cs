0ed422ce766d53661c6094db546b7073cf52bf31
nick <nick121212@126.com>
2014-08-08 18:27:14 +0800

    33
    33333

---
+++ desktop/Core/XIMALAYA.PCDesktop.Core.Models/Search/SearchResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XIMALAYA.PCDesktop.Core.Models.Search
+{
+    /// <summary>
+    /// 搜索返回数据
+    /// </summary>
+    public class SearchResult : BaseResult
+    {
+
+    }
+}
